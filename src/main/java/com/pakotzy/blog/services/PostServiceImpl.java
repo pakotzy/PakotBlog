f5b5c080b15f3fa8650a5d3b329db2c63b747724
Pavlo Kotelnytskyi <pakotzy@gmail.com>
2018-07-04 15:38:12 +0300

    Mock data

--- src/main/java/com/pakotzy/blog/services/PostServiceImpl.java
+++ src/main/java/com/pakotzy/blog/services/PostServiceImpl.java
@@ -1,20 +1,35 @@
 package com.pakotzy.blog.services;
 
 import com.pakotzy.blog.models.Post;
+import com.pakotzy.blog.models.User;
 import com.pakotzy.blog.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostServiceImpl implements PostService {
+	// TODO: Connect to the database
+	private List<Post> posts = new ArrayList<>();
+
+	@PostConstruct
+	private void configure() {
+		User user = new User("pakotzy", "password");
+
+		posts.add(new Post(1L, "First post", "Body of the first post", user, LocalDateTime.now()));
+		posts.add(new Post(1L, "Second post", "Body of the second post", user, LocalDateTime.now()));
+	}
+
 	@Autowired
 	PostRepository repository;
 
 	@Override
 	public List<Post> findAll() {
-		return repository.findAll();
+		return posts;
 	}
 
 	@Override
